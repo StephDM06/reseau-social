7f1e2a50a026b322a8c17085f65406f11f224e75
StephDM06 <dumasmiton.stephane@ecomail.fr>
2022-03-21 14:53:16 +0000

    commit steph2

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,10 +4,13 @@
     <p>L'équipe des bras cassés</p>
   </div>
 </template>
-<script></script>
+<script>
+export default {};
+</script>
 
 <style scoped>
 #about {
   text-align: center;
+  background-color: brown;
 }
 </style>
