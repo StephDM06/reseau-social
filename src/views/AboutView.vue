290ff1f124aad6adbc82573e5b52581dd0e9a152
StephDM06 <dumasmiton.stephane@ecomail.fr>
2022-03-21 14:30:17 +0000

    commit steph1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Création du site</h1>
+    <p>L'équipe des bras cassés</p>
   </div>
 </template>
+<script></script>
+
+<style scoped>
+#about {
+  text-align: center;
+}
+</style>
---
+++ src/views/filActu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="principale">
+    <h1>Votre fil d'actualité</h1>
+    <h3>Les derniers post</h3>
+  </div>
+</template>
+
+<script>
+const DataPost = {
+  data() {
+    return {
+      post: [],
+    };
+  },
+};
+
+export default DataPost;
+</script>
+
+<style scoped>
+#principale {
+  text-align: center;
+}
+</style>
