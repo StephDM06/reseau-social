886af6dd25b7114eaac6d8c88954d16f63a3ac00
juanmalisa <juanmalisa@gmail.com>
2022-03-22 09:54:22 +0100

    commit juan

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,43 +1,35 @@
-
-
 <template>
-    <div class="postContainer">
-        <div id="post">
-          <span>Posted by:{{user}}</span>
-          <span>Posted the: {{date}}</span>
-          <span>{{content}}</span>
-          <span class="buttonLike">like {{like}}</span>
-        </div>
+  <div class="postContainer">
+    <div id="post">
+      <span>Posted by:{{ user }}</span>
+      <span>Posted the: {{ date }}</span>
+      <span>{{ content }}</span>
+      <span class="buttonLike">like {{ like }}</span>
     </div>
+  </div>
 </template>
 
-
 <script>
-
-    const Post = {
-        data(){
-            return{
-                postTableau:[],
-                userName: "",
-                postDate: "",
-                postContent:"",
-                likeCount:"",
-            };
-            
-        },
-
-        props: {
-        like: Number,
-        user: String,
-        date: Date,
-        content: String,
+const Post = {
+  data() {
+    return {
+      postTableau: [],
+      userName: "",
+      postDate: "",
+      postContent: "",
+      likeCount: "",
+    };
   },
 
-    };
+  props: {
+    like: Number,
+    user: String,
+    date: Date,
+    content: String,
+  },
+};
 
 export default Post;
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
