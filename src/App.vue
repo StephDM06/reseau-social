49817737231fc2d795ac9a2e45f9ec0b0232f8f9
AbderrezakWEBDEV <razik.bouiche@gmail.com>
2022-03-22 09:29:28 +0100

    nouvelle nav barre

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,42 @@
 <template>
-  <nav>
+  <nav class="navbar">
+    <h1>ACTU-DEV-WEB</h1>
     <router-link to="/">Accueil</router-link>
     <router-link to="/Profil">Profil</router-link>
     <router-link to="/about">A propos</router-link>
+    <div>
+      <input type="text" placeholder="Recherche" />
+      <button class="like">Recherche</button>
+    </div>
   </nav>
   <router-view />
 </template>
 
 <script></script>
 
-<style scoped></style>
+<style scoped>
+.navbar {
+  background-color: aqua;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  text-align: center;
+  color: blue;
+  border-bottom: 3px solid blue;
+  border-radius: 5px;
+}
+.first {
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  text-align: center;
+}
+input {
+  border: none;
+  align-items: center;
+}
+.like {
+  border: none;
+  color: blue;
+}
+</style>
---
+++ src/components/creation.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="card">
+    <h1 class="card_title" v-if="mode == 'login'">Connexion</h1>
+    <h1 class="card_title" v-else>inscritption</h1>
+    <p class="card_subtitle" else-if>
+      Tu'as déja un compte<span
+        class="card_action"
+        @click="switchtocreatacompte()"
+        >se connecter</span
+      >
+    </p>
+    <p class="card_subtitle" v-if="mode == 'login'">
+      Tu n'a pas encore de compte?<span
+        class="card_action"
+        @click="switchtologin()"
+        >créer un compte</span
+      >
+    </p>
+    <div class="form-row">
+      <input
+        v-model="email"
+        class="form-input"
+        type="text"
+        placeholder="adresse mail"
+      />
+    </div>
+    <div class="form-row" v-if="mode == 'create'">
+      <input
+        v-model="prenom"
+        class="form-input"
+        type="text"
+        placeholder="prenom"
+      />
+      <input v-model="nom" class="form-input" type="text" placeholder="nom" />
+    </div>
+    <div class="form-row">
+      <input
+        v-model="password"
+        class="form-input"
+        type="password"
+        placeholder="mot de passe"
+      />
+    </div>
+    <div class="form-row">
+      <button class="desabled" v-if="mode == 'login'">Connexion</button>
+      <button
+        class="desabled"
+        :class="{ 'button-desabled': !valitedFields }"
+        v-else
+      >
+        Créer mon compte
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      mode: "login",
+    };
+  },
+  methods: {
+    switchtocreatacompte: function () {
+      this.mode = "create";
+    },
+    switchtologin: function () {
+      this.mode = "login";
+    },
+  },
+  computed: {
+    valitedFields: function () {
+      if (this.mode == "create") {
+        if (
+          this.email != "" &&
+          this.prenom != "" &&
+          this.nom != "" &&
+          this.password != ""
+        ) {
+          return true;
+        } else {
+          return flase;
+        }
+      } else {
+        if (this.email != "" && this.password != "") {
+          return true;
+        }
+        return false;
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
