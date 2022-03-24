8bc637c25ed82d537ac690151163a24c710e4c7a
EliotBelz <eliot.06@live.fr>
2022-03-24 16:01:49 +0100

    modif eliot 16h01 style

--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -44,33 +44,3 @@ const ProfilDonnees = {
 
 export default ProfilDonnees;
 </script>
-
-<style scoped>
-#profil {
-  font-family: lato;
-  text-align: center;
-  padding-top: 5%;
-}
-#donnees {
-  font-family: lato;
-  width: 350px;
-  text-align: left;
-  margin-left: 40%;
-  padding-top: 20px;
-}
-#main-profil {
-  font-display: flex;
-  text-align: center;
-  width: 50%;
-  height: 400px;
-  margin-left: 25%;
-  margin-top: 10%;
-  background-color: #9efff9;
-  border: solid 4px #00857d;
-  border-radius: 3px;
-}
-
-#profil h1 {
-  color: red;
-}
-</style>
